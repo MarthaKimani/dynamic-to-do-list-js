dacf75ccaa582784cb0469c38dc086c53db3aff2
MarthaKimani <marthakimani423@gmail.com>
2025-08-01 15:29:29 +0300

    fixed an error

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const todoForm = document.getElementById("todo-form");
     const todoInput = document.getElementById("todo-input");
     const todoList = document.getElementById("todo-list");
+// Select DOM elements
+const addButton = document.getElementById("add-task-btn");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
 
     // Handle form submit
     todoForm.addEventListener("submit", (e) => {
